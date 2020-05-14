15f99efbe4c400715b25c39a9113dfb7517fc009
Michal Povinsky <antihutka@gmail.com>
2020-05-14 02:29:43 +0200

    Track average message length

---
+++ sql/20200514-msglen.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `chat_uniqueness` ADD `avg_len` DOUBLE NULL AFTER `last_count_valid`;
+
--- update_uniq.py
+++ update_uniq.py
@@ -21,9 +21,9 @@ def add_new_chats(db, cur):
 
 get_chats_q = """
 SELECT * FROM (
-  SELECT convid, message_count, new_messages, age, CAST((100 * new_messages)/(100+message_count) + age / (1440 * 7)  AS DOUBLE) AS score, COALESCE(uniqueness, -1) AS uniqueness, chatname
+  SELECT convid, message_count, new_messages, age, CAST((100 * new_messages)/(100+message_count) + age / (1440 * 7)  AS DOUBLE) AS score, COALESCE(uniqueness, -1) AS uniqueness, avg_len, chatname
   FROM (
-    SELECT convid, message_count, message_count - last_count AS new_messages, TIMESTAMPDIFF(MINUTE, last_update, CURRENT_TIMESTAMP) AS age, uniqueness, long_name as chatname
+    SELECT convid, message_count, message_count - last_count AS new_messages, TIMESTAMPDIFF(MINUTE, last_update, CURRENT_TIMESTAMP) AS age, uniqueness, avg_len, long_name as chatname
     FROM chat_uniqueness LEFT JOIN chat_counters USING (convid) LEFT JOIN chatinfo_current USING (convid) LEFT JOIN chatinfo_v USING (chatinfo_id) WHERE sent=0
   ) a
 ) b WHERE score > 0.1 OR uniqueness < 0 ORDER BY score DESC LIMIT 10;
@@ -41,25 +41,25 @@ def update_step(db, cur):
     print("No chats to update")
     return 60
   for i in chats_to_update:
-    print("Chat: %16d New: %6d / %6d updated: %6d minutes ago score: %4.2f uniq: %.3f %s" % i)
+    print("Chat: %16d New: %6d / %6d updated: %6d minutes ago score: %4.2f uniq: %.3f len: %.1f %s" % i)
   convid = chats_to_update[0][0]
   chatname = chats_to_update[0][6]
   print("Updating stats for %d %s" % (convid, chatname))
-  cur.execute("SELECT COALESCE(SUM(IF(count=1, 1, 0)) / COUNT(*), 0) FROM chat LEFT JOIN chat_hashcounts ON hash=UNHEX(SHA2(text, 256)) "
+  cur.execute("SELECT COALESCE(SUM(IF(count=1, 1, 0)) / COUNT(*), 0), COUNT(*), AVG(LENGTH(text)) FROM chat LEFT JOIN chat_hashcounts ON hash=UNHEX(SHA2(text, 256)) "
     "WHERE chat.sent = 0 AND chat.convid=%s AND text NOT IN (SELECT DISTINCT emoji FROM stickers)", (convid,))
-  uniqueness = cur.fetchone()[0]
+  (uniqueness, msgcount_v, avglen) = cur.fetchone()
   cur.execute("SELECT message_count FROM chat_counters WHERE convid = %s AND sent = 0", (convid,))
   msgcount = cur.fetchone()[0]
-  cur.execute("SELECT COUNT(*) FROM chat WHERE sent=0 AND convid=%s AND text NOT IN (SELECT DISTINCT emoji FROM stickers)", (convid,))
-  msgcount_v = cur.fetchone()[0]
+  print('avglen %f' % (avglen))
   db.commit()
   #print("Updated uniqueness of chat %s from %.3f to %.3f count=%d countv=%d" % (chatname, chats_to_update[0][5], uniqueness, msgcount, msgcount_v))
   cur.execute("UPDATE chat_uniqueness SET "
     "uniqueness=%s, "
     "last_count=%s, "
     "last_count_valid=%s, "
+    "avg_len=%s, "
     "last_update = CURRENT_TIMESTAMP "
-    "WHERE convid=%s", (uniqueness, msgcount, msgcount_v, convid))
+    "WHERE convid=%s", (uniqueness, msgcount, msgcount_v, avglen, convid))
   db.commit()
   endtime = time.time()
   elaps = endtime-starttime
@@ -70,8 +70,8 @@ def update_step(db, cur):
     varsleep = varsleep - 1
   sleeptime = (elaps * 10 + varsleep) / max(0.25, chats_to_update[0][4])
   #print("Done updating stats for %d %s (took %.3f) (sleeping for %6.3f)" % (convid, chatname, elaps, sleeptime))
-  print("Updated %s from %.3f to %.3f (%.6f) cnt=%d/%d score %.3f took %.3f slp %6.3f" %
-       (chatname, chats_to_update[0][5], uniqueness, float(uniqueness) - chats_to_update[0][5], msgcount_v, msgcount, chats_to_update[0][4], elaps, sleeptime))
+  print("Updated %s from %.3f to %.3f (%.6f) cnt=%d/%d score %.3f avglen %.1f took %.3f slp %6.3f" %
+       (chatname, chats_to_update[0][5], uniqueness, float(uniqueness) - chats_to_update[0][5], msgcount_v, msgcount, chats_to_update[0][4], avglen, elaps, sleeptime))
   return sleeptime
 
 while True:
