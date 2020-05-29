ad041a2ab066cfc6ae4d6755360a440c7fbab0f1
Michal Povinsky <antihutka@gmail.com>
2020-05-29 14:42:28 +0200

    Add options2 SQL update

---
+++ sql/20200527-options2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `options2` (
+`convid` BIGINT NOT NULL ,
+`sticker_prob` DOUBLE NULL DEFAULT NULL ,
+`reply_prob` DOUBLE NULL DEFAULT NULL ,
+`admin_only` TINYINT NULL DEFAULT NULL ,
+`blacklisted` TINYINT NULL DEFAULT NULL ,
+`silent_commands` TINYINT NULL DEFAULT NULL ,
+`is_bad` INT NULL DEFAULT NULL ,
+`is_hidden` INT NULL DEFAULT NULL ,
+PRIMARY KEY (`convid`)) ENGINE = ROCKSDB; 
+
